28cac5d1d2a0341c617d0c5391e907008959e66e
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-06 13:35:09 +0300

    Create clear.js

---
+++ commands/moderation/clear.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "clear",
+    category: "moderation",
+    aliases: ["c"],
+    description: "clears chat (max messages 99)",
+    usage: "<count>",
+    run: (client, message, args) => {
+        message.delete();
+        
+        if(message.member.hasPermission("MANAGE_MESSAGES")) {
+    if(args[0])
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+    })}else message .channel.send("You don't have required permissions to access this command!");
+}
+}
