89c9ff28a240ff0a0c42e15e1f0385fc2a5d3737
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-06 14:08:15 +0300

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -6,7 +6,7 @@ module.exports = {
     name: "ban",
     category: "moderation",
     description: "bans the member",
-    usage: "<id | mention>",
+    usage: "[id | mention] [reason]",
     run: async (client, message, args) => {
         const logChannel = message.guild.channels.find(c => c.name === "logs") || message.channel;
 
